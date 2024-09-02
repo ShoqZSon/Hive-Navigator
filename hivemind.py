c32a2a015169da20e78fbe0bd89fcf52db3646ba
tihoe005 <tim.hoenings@stud.hn.de>
2024-09-02 12:08:02 +0200

    Additions: - fix-test: switched hivemind.hivemind.listenToConn() and start_listening_for_locations()

--- hivemind.py
+++ hivemind.py
@@ -4,9 +4,9 @@ import threading
 
 hivemind = Hivemind('192.168.56.106',5672,'192.168.56.102',57920)
 
+webserverThread = threading.Thread(target=hivemind.listenToConn())
 # hivemind listens to incoming coordinates from the bots
 brokerThread = threading.Thread(target=hivemind.start_listening_for_locations())
-webserverThread = threading.Thread(target=hivemind.listenToConn())
 
 brokerThread.start()
 webserverThread.start()
