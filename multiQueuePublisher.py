c3eea80bf086eaec2a11020218ec8ace5d22e18d
ShoqZSon <heoningstim@web.de>
2024-10-14 16:46:22 +0200

    deleted unused scripts and files

--- multiQueuePublisher.py
+++
@@ -1,8 +0,0 @@
-from publisher import Publisher
-
-
-class MultiQueuePublisher(Publisher):
-    def __init__(self, messageBrokerHost, messageBrokerPort, exchange='',routing_key='',queue=''):
-        super().__init__(messageBrokerHost, messageBrokerPort, exchange,queue)
-        self.routing_key = routing_key
-
